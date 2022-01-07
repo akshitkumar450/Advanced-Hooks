947e69462acd4e78041318e505148d22b4a07858
Akshit Kumar <akshitkumar450@gmail.com>
2022-01-07 15:13:51 +0530

    useEffect-2

--- src/UseEffect.js
+++ src/UseEffect.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
 
+// if the function is not dependent on any state or props and we want to use it in useEffect ,,define that function outside the component
+// in this way on every render the function will not re-initialzed
+
+const logMessage = () => {
+  console.log("hello");
+};
+
 function UseEffectEx() {
   const [val, setVal] = useState(false);
-  //   whenever our component rerenders the logMessage function is re initialized (react thing) and  and our useEffect dependency array will think that our function is changed  and it will run the useEffect
-  const logMessage = () => {
-    console.log("hello");
-  };
+  // whenever our component rerenders the logMessage function is re initialized (react thing) and  and our useEffect dependency array will think that our function is changed  and it will run the useEffect
+
   // if we want to log logMessage function when ever the function changes ,
   useEffect(() => {
     logMessage();
-  }, [logMessage]);
+  }, []);
   return (
     <div>
       <h2>useEffect</h2>
