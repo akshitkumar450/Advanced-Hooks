cbe026f6aa9fc9be90c68a76ea9e3f939457bb9c
Akshit Kumar <akshitkumar450@gmail.com>
2022-01-08 17:24:50 +0530

    usecallback hook-2

--- src/UseCallback1.js
+++ src/UseCallback1.js
@@ -6,15 +6,22 @@ function UseCallback1() {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
 
-  const increment1 = () => setCount1(count1 + 1);
-  const increment2 = () => setCount2(count2 + 1);
+  //   these will be also redefine at every single render and will generate a new functionand thus will be added to set as at every render it is a new function
+
+  //   can be avoided by useCallback
+
+  //   the function inside the useCallback will redefine if the dependency is changes otherwise it will use the memoize verison of the function
+  const increment1 = useCallback(() => setCount1(count1 + 1), [count1]);
+  const increment2 = useCallback(() => setCount2(count2 + 1), [count2]);
 
   //   this function will be generated new at every single time and it will be added in the set even if the function is same ,,but at every render its reference in the memory is not same
 
   //   this can be avoided by useCallback hook which is used to memoize the function
   const logName = useCallback(() => console.log("render"), []);
 
-  functions.add(logName);
+  functions.add(increment1);
+  functions.add(increment2);
+  //   functions.add(logName);
   console.log(functions);
 
   return (
